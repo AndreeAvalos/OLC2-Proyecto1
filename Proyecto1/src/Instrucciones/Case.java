9d1670635376cc228d2f20f893600c13dba718aa
AndreeAvalos <aavalosoto@gmail.com>
2019-12-15 22:48:00 -0600

    Sentencia Switch

--- Proyecto1/src/Instrucciones/Case.java
+++ Proyecto1/src/Instrucciones/Case.java
@@ -5,10 +5,60 @@
  */
 package Instrucciones;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author Andree
  */
-public class Case {
+public class Case{
+    public enum TipoCase{
+        CASE,
+        DEFAULT;
+    }
+    Operacion expresion;
+    LinkedList<Instruccion> contenido;
+    TipoCase tipo_case;    
+    int line,column;
+
+    public Case(Operacion expresion, LinkedList<Instruccion> contenido, int line, int column) {
+        this.expresion = expresion;
+        this.contenido = contenido;
+        this.line = line;
+        this.column = column;
+        this.tipo_case = TipoCase.CASE;
+    }
+
+    public Case(LinkedList<Instruccion> contenido, int line, int column) {
+        this.contenido = contenido;
+        this.line = line;
+        this.column = column;
+        this.tipo_case = TipoCase.DEFAULT;
+    }
+
+    public Operacion getExpresion() {
+        return expresion;
+    }
+
+    public LinkedList<Instruccion> getContenido() {
+        return contenido;
+    }
+
+    public TipoCase getTipo_case() {
+        return tipo_case;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
     
+    
+    
+    
+
 }
--- Proyecto1/src/Instrucciones/Sentencia_Switch.java
+++ Proyecto1/src/Instrucciones/Sentencia_Switch.java
@@ -5,40 +5,130 @@
  */
 package Instrucciones;
 
+import Instrucciones.Case.TipoCase;
 import Tabla_Simbolos.TablaDeSimbolos;
+import Tabla_Simbolos.Tipo_Retorno;
+import java.util.LinkedList;
 
 /**
  *
  * @author Andree
  */
 public class Sentencia_Switch implements Instruccion {
-    
-    Operacion expresion;
 
+    Operacion expresion;
+    LinkedList<Case> casos;
+    int line, column;
+
+    public Sentencia_Switch(Operacion expresion, LinkedList<Case> casos, int line, int column) {
+        this.expresion = expresion;
+        this.casos = casos;
+        this.line = line;
+        this.column = column;
+    }
 
     @Override
     public int getLine() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.column;
     }
 
     @Override
     public int getColumn() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.line;
     }
 
     @Override
     public Object Ejecutar(TablaDeSimbolos ts) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//        string opcion = instruccion_switch.Expresion.Ejecutar(ts).ToString();
+        Case caso_default = null;
+
+        for (Case item : casos) {
+            if (item.getTipo_case() == TipoCase.DEFAULT) {
+                caso_default = item;
+            }
+        }
+
+        TablaDeSimbolos tabla_local = new TablaDeSimbolos();
+        String opcion = expresion.Ejecutar(ts).toString();
+        tabla_local.setPadre(ts);
+        boolean tiene_break = true;
+        // corren todos los casos excepto el default
+        for (Case case_actual : casos) {
+            if (case_actual.getTipo_case() != TipoCase.DEFAULT) {
+                Object valor = case_actual.expresion.Ejecutar(ts).toString();
+                if (valor.equals(opcion) || !tiene_break) {
+                    for (Instruccion item : case_actual.getContenido()) {
+                        switch (item.getType()) {
+                            case BREAK:
+                                return null;
+                            case SEGUIR:
+                                return new Tipo_Retorno(Tipo.ETIQUETA_SIGUE, null);
+                            case RETURN:
+                                return item.Ejecutar(tabla_local);
+                            default:
+                                Object result = item.Ejecutar(tabla_local);
+                                if (result != null) {
+                                    try {
+                                        Tipo_Retorno etiqueta = (Tipo_Retorno) result;
+                                        if (etiqueta.getEtiqueta() == Tipo.ETIQUETA_RETURN) {
+                                            return etiqueta;
+                                        }
+                                        if (etiqueta.getEtiqueta() == Tipo.ETIQUETA_SIGUE) {
+                                            return etiqueta;
+                                        } else {
+                                            return null;
+                                        }
+                                    } catch (Exception e) {
+                                    }
+                                }
+                                break;
+                        }
+                    }
+                    tiene_break = false;
+                }
+            }
+        }
+        //si llega aqui es por que no encontro conicidencia
+        if (caso_default != null) {
+            for (Instruccion item : caso_default.getContenido()) {
+                switch (item.getType()) {
+                    case BREAK:
+                        return null;
+                    case SEGUIR:
+                        return new Tipo_Retorno(Tipo.ETIQUETA_SIGUE, null);
+                    case RETURN:
+                        return item.Ejecutar(tabla_local);
+                    default:
+                        Object result = item.Ejecutar(tabla_local);
+                        if (result != null) {
+                            try {
+                                Tipo_Retorno etiqueta = (Tipo_Retorno) result;
+                                if (etiqueta.getEtiqueta() == Tipo.ETIQUETA_RETURN) {
+                                    return etiqueta;
+                                }
+                                if (etiqueta.getEtiqueta() == Tipo.ETIQUETA_SIGUE) {
+                                    return etiqueta;
+                                } else {
+                                    return null;
+                                }
+                            } catch (Exception e) {
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+        //si llega aqui es por que tampoco encontro default
+        return null;
     }
 
     @Override
     public void Recolectar(TablaDeSimbolos ts) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public Tipo getType() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return Tipo.SENTENCIA_SWITCH;
     }
-    
+
 }
